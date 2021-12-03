2dfc4c0785b8795bf46b5191a9062f0a09d46cf0
KDOS2 <52287145+KDOS2@users.noreply.github.com>
2021-12-03 14:34:04 -0500

    Add files via upload

---
+++ JsFile.js
@@ -0,0 +1,8 @@
+﻿function validarNumeros(e) {
+    var key = window.Event ? e.which : e.keyCode
+    return (key >= 48 && key <= 57) || key == 45
+}
+
+function Editar(valor) {
+    __doPostBack('Editar', valor);
+}
